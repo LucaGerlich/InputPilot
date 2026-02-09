e264df1c661d21348596a2851ae1f213ac260892
Luca Gerlich <l.gerlich@fitseveneleven.de>
2026-02-09 11:37:14 +0100

    Initial Commit

---
+++ InputPilot/InputPilotApp.swift
@@ -0,0 +1,32 @@
+//
+//  InputPilotApp.swift
+//  InputPilot
+//
+//  Created by Luca on 09.02.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct InputPilotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ InputPilotTests/InputPilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  InputPilotTests.swift
+//  InputPilotTests
+//
+//  Created by Luca on 09.02.26.
+//
+
+import Testing
+@testable import InputPilot
+
+struct InputPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
